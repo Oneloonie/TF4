690f904e7c6ae05bea523930cb6ca903e1d7eb64
Oneloonie <One.loonie@protonmail.com>
2026-04-22 19:08:15 +0000

    fix: use absolute DB path

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 BASE_DIR= os.path.dirname(os.path.abspath(__file__))
 DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'TSQL2012.db')}"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine)
 Base=declarative_base()
 
